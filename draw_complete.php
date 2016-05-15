ae764f6ed7b6ad9ace9a1bb8aaa42e788a60fcc4
kjin <kkj9@cornell.edu>
2016-05-15 18:40:42 +0000

    draw completed php launcher

---
+++ draw_complete.php
@@ -0,0 +1,7 @@
+<html>
+<head><title>Drawing phase completed</title></head>
+<body>
+All drawing tasks in this batch have been completed.<br />
+Please help us rate the drawings: <a href="eval.php">Click Here</a>
+</body>
+</html>
